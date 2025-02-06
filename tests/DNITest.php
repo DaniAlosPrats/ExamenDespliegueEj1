af388cb04a9aacdae6db80e8b245cd3892ab6686
DANIEL ALOS PRATS <daalpr@alumnatflorida.es>
2025-02-06 10:03:47 +0100

    5 commit

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 use App\DNI;
 class DNITest extends TestCase {
     
- public function testSuma() {
+ public function testDNI() {
  $calc = new DNI();
  $this-> assertEquals(Z, $calc->getDNIConLetra(12345678));
  }
